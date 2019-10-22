e2b8f38c25e557a2d7ee7d113c355752a4ef7e2e
ushlitsa <56569249+ushlitsa@users.noreply.github.com>
2019-10-22 16:27:11 +0300

    2nd part of test

--- pages/base_page.py
+++ pages/base_page.py
@@ -27,6 +27,7 @@ class BasePage():
     def enter_text(self, locator, text):
         self.get_element(locator).clear()
         self.get_element(locator).send_keys(text)
-
-    def get_text(self, locator):
-         return self.get_element(locator).text
+        
+    def get_text(self, locator, text):
+        expected_condition = ec.text_to_be_present_in_element(locator, text)
+        return WebDriverWait(self.driver, 10).until(expected_condition, message="Unable to locate element")
